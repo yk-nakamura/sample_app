738bb71304b9159ac96833fb0929ee49127fee07
yk-nakamura <yu.s2pg.ki@gmail.com>
2014-08-29 14:07:42 +0900

    test for page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			# expect(page).to have_title("Home")でも良い
+		end
 	end
 
 	describe "Help page" do
@@ -13,12 +19,22 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
 end
