cfba96ef9baddf338aec3d4240875adcddc33051
yk-nakamura <yu.s2pg.ki@gmail.com>
2014-08-29 14:54:39 +0900

    Exercise (3.5)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe "Static pages" do
+
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
 	describe "Home page" do # RSpecは""で囲まれた文字列を解釈しないので、わかりやすい説明をかく
 		it "should have the content 'Sample App'" do	# shold have～と書くことでitと整合性がとれる
 			visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("#{base_title} | Home")
 			# expect(page).to have_title("Home")でも良い
 		end
 	end
@@ -22,7 +25,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -34,7 +37,19 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
+		end
+	end
+	
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+	
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 end
