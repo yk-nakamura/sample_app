8c5eda8cca4261dcb65cef12618d25ef1206f806
yk-nakamura <yu.s2pg.ki@gmail.com>
2014-09-04 09:03:16 +0900

    Request the right user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
 
 	def show
 		@user = User.find(params[:id])
@@ -25,7 +26,6 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update_attributes(user_params)
 			flash[:success] = "Profile updated"
 			redirect_to @user
@@ -44,5 +44,10 @@ class UsersController < ApplicationController
 
 		def signed_in_user
 			redirect_to signin_url, notice: "Please sign in." unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
 		end
 end
