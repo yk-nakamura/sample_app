891b4e8c091508815257cbd6e5b517533817573f
yk-nakamura <yu.s2pg.ki@gmail.com>
2014-09-05 13:56:09 +0900

    Create microposts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,10 +54,10 @@ class UsersController < ApplicationController
 
 		# Before actions
 
-	  def signed_in_user
-			store_location
-			redirect_to signin_url, notice: "Please sign in." unless signed_in?
-	  end
+	  #def signed_in_user
+		#	store_location
+		#	redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	  #end
 
 		def correct_user
 			@user = User.find(params[:id])
